f4c88f7563c2d33636f5197c1faa459196c07a36
Prabin Kayastha <princekayastha@gmail.com>
2019-11-26 00:28:22 +0545

    Rendered the ship image into the game screen by creating ship class

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,15 +3,26 @@ import sys
 import pygame
 from pygame.locals import *
 
+
+from settings import Settings
+from ship import Ship
+
+
+
 class AlienInvasion:
     '''Overall Class to manage game assets and behaviour.'''
 
     def __init__(self):
         '''Initialize the game, and create game resources.'''
         pygame.init()
+        self.settngs = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((self.settngs.screen_width, self.settngs.screen_height))
         pygame.display.set_caption('Alien Invasion')
+        self.ship = Ship(self)
+
+        # set background color
+        self.bg_color = (255, 255, 255)
 
     def run_game(self):
         '''Start the main loop to run the game'''
@@ -21,6 +32,11 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            # Redraw the screen during each pass through the loop
+            self.screen.fill(self.settngs.bg_color)
+            self.ship.blitme()
+
+            
             # Make the most recently drawn screen visible
             pygame.display.flip()
 
