77aa85b3c213f8625ffcfedce98dceb01ee09cad
Prabin Kayastha <princekayastha@gmail.com>
2020-01-20 19:00:40 +0545

    added codes to remove the disssappeared bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -33,6 +33,13 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self.bullets.update()
+
+            # Get rid of bullets that are out of screen.
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+            print(len(self.bullets))
+
             self._update_screen()
 
 
