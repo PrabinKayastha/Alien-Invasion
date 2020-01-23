4df990d1b9f315c167a1bad6ae94f3d750bc0a09
Prabin Kayastha <princekayastha@gmail.com>
2020-01-23 18:48:06 +0545

    added high score dashboard

--- game_stats.py
+++ game_stats.py
@@ -6,9 +6,11 @@ class GameStats:
         self.settings = ai_game.settings
         self.reset_stats()
 
-
         # Start Alien Invasion in an inactive status.
         self.game_active = False
+
+        # Highscore should never be reset.
+        self.high_score = 0
 
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
