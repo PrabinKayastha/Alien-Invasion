d1be7b5584577e841e5ccc111d0efc674f484d39
Prabin Kayastha <princekayastha@gmail.com>
2020-01-23 17:07:00 +0545

    disabled the play button while the game is in active mode.

--- alien_invasion.py
+++ alien_invasion.py
@@ -68,8 +68,19 @@ class AlienInvasion:
                 
     def _check_play_button(self, mouse_pos):
         """Start a new game when the player clicks play."""
-        if self.play_button.rect.collidepoint(mouse_pos):
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active:
+            # Reset the game statistics.
+            self.stats.reset_stats()
             self.stats.game_active = True
+
+            # Get rid of remaining aliens and bullets.
+            self.aliens.empty()
+            self.bullets.empty()
+
+            # Create a new fleet and center the ship.
+            self._create_fleet()
+            self.ship.center_ship()
 
 
     def _check_key_down_event(self, event):
