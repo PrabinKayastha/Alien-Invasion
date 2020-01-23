68b334a96006dbd8e56859a7b28bd1d6b0b49f83
Prabin Kayastha <princekayastha@gmail.com>
2020-01-23 15:34:59 +0545

    Start Alien Invasion in an inactive status.

--- game_stats.py
+++ game_stats.py
@@ -7,8 +7,8 @@ class GameStats:
         self.reset_stats()
 
 
-        # Start Alien Invasion in an active.
-        self.game_active = True
+        # Start Alien Invasion in an inactive status.
+        self.game_active = False
 
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
