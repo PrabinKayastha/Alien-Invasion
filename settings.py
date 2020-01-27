12c7563c818c93cabcd096cfca215f6747bc35b8
Prabin Kayastha <princekayastha@gmail.com>
2020-01-27 11:39:35 +0545

    adjusted the game settings

--- settings.py
+++ settings.py
@@ -6,11 +6,11 @@ class Settings:
 
         """Screen settings"""
         self.screen_width = 1200
-        self.screen_height = 700
+        self.screen_height = 800
         self.bg_color = (230, 230, 230)
 
         """Ship Settings"""
-        self.ship_horizontal_speed = 2.0
+        self.ship_horizontal_speed = 2.25
         self.ship_vertical_speed = 1.5
         self.ship_limit = 3
 
@@ -19,11 +19,11 @@ class Settings:
         self.bullet_height = 15
         self.bullet_width = 5
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 5
+        self.bullets_allowed = 10
 
         """Alien Settings"""
         self.alien_speed = 1.0
-        self.fleet_drop_speed = 300
+        self.fleet_drop_speed = 10.0
         # fleet_direction of 1 represents right; -1 represents left.bit_length.
         self.fleet_direction = 1
 
